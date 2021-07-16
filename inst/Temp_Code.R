e00fbbfe07e78f182229bd804a924a580c00b08f
Steven J. Pierce <pierces1@msu.edu>
2021-07-16 14:45:18 -0400

    Add temporary code we may use in responding to peer review.

---
+++ inst/Temp_Code.R
@@ -0,0 +1,319 @@
+TIDNEW <-
+  IDNEW %>%
+  select(HXCat12_1_After, HXCat12_2_After, HXCat12_3_After, HXCat12_4_After,
+         HXCat12_5_After, HXCat12_6_After, HXCat12_7_After, HXCat12_8_After,
+         HXCat12_9_After, HXCat12_10_After, HXCat12_11_After, HXCat12_12_After,
+         HXCat12_Count_After)
+
+names(TIDNEW) <- c(paste0("HCA_", 1:12), "HCA_Count")
+
+
+# Aggregate to unique patterns of incident counts across HXCat12_1_After to
+# HXCat12_12_After, plus HXCat12_Count_After and compute frequency of each
+# pattern.
+head(TIDNEW)
+DTIDNEW <-
+  TIDNEW %>%
+  group_by(HCA_1, HCA_2, HCA_3, HCA_4, HCA_5, HCA_6, HCA_7, HCA_8, HCA_9,
+           HCA_10, HCA_11, HCA_12, HCA_Count) %>%
+  summarise(N = n()) %>%
+  # Sort in descending order by pattern frequency and crime category count for
+  # the pattern
+  arrange(desc(N), desc(HCA_Count))
+
+nrow(DTIDNEW)  # No. unique patterns
+head(DTIDNEW)
+
+# Aggregate to unique patterns of binary transforms of HXCat12_1_After to
+# HXCat12_12_After, plus HXCat12_Count_After and compute frequency of each
+# pattern.
+head(TIDNEW)
+DTIDNEW <-
+  TIDNEW %>%
+  mutate(HCA_1 = if_else(HCA_1 > 0, true = 1, false = 0),
+         HCA_2 = if_else(HCA_2 > 0, true = 1, false = 0),
+         HCA_3 = if_else(HCA_3 > 0, true = 1, false = 0),
+         HCA_4 = if_else(HCA_4 > 0, true = 1, false = 0),
+         HCA_5 = if_else(HCA_5 > 0, true = 1, false = 0),
+         HCA_6 = if_else(HCA_6 > 0, true = 1, false = 0),
+         HCA_7 = if_else(HCA_7 > 0, true = 1, false = 0),
+         HCA_8 = if_else(HCA_8 > 0, true = 1, false = 0),
+         HCA_9 = if_else(HCA_9 > 0, true = 1, false = 0),
+         HCA_10 = if_else(HCA_10 > 0, true = 1, false = 0),
+         HCA_11 = if_else(HCA_11 > 0, true = 1, false = 0),
+         HCA_12 = if_else(HCA_12 > 0, true = 1, false = 0)) %>%
+  group_by(HCA_1, HCA_2, HCA_3, HCA_4, HCA_5, HCA_6, HCA_7, HCA_8, HCA_9,
+           HCA_10, HCA_11, HCA_12, HCA_Count) %>%
+  summarise(N = n()) %>%
+  # Sort in descending order by pattern frequency and crime category count for
+  # the pattern
+  arrange(HCA_Count, desc(N)) %>%
+  ungroup() %>%
+  mutate(Cum_N = cumsum(N),
+         Pct = 100*N/sum(N),
+         Cum_Pct = 100*Cum_N/sum(N))
+
+nrow(DTIDNEW)  # No. unique patterns
+head(DTIDNEW)
+
+
+# For the after period, get total number and percent of offenders with 1 or more
+# incidents, plus the total number of incidents arrested, charged, or convicted
+
+IDNEW %>%
+  select(HXCat12_1_After) %>%
+  filter(HXCat12_1_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_1_After)) %>%
+  mutate(Category = "Arson",
+         Group = "Property Crimes",
+         SortOrder = 8) ->
+  HCA_1
+
+IDNEW %>%
+  select(HXCat12_2_After) %>%
+  filter(HXCat12_2_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_2_After)) %>%
+  mutate(Category = "Assault, Domestic Violence",
+         Group = "Violent Non-Sexual Crimes",
+         SortOrder = 3) ->
+  HCA_2
+
+IDNEW %>%
+  select(HXCat12_3_After) %>%
+  filter(HXCat12_3_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_3_After)) %>%
+  mutate(Category = "Assault, non-Domestic Violence",
+         Group = "Violent Non-Sexual Crimes",
+         SortOrder = 4) ->
+  HCA_3
+
+IDNEW %>%
+  select(HXCat12_4_After) %>%
+  filter(HXCat12_4_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_4_After)) %>%
+  mutate(Category = "Burglary",
+         Group = "Property Crimes",
+         SortOrder = 9) ->
+  HCA_4
+
+# IDNEW %>%
+#   select(HXCat12_5_After) %>%
+#   filter(HXCat12_5_After > 0) %>%
+#   ungroup() %>%
+#   summarise(N = n(),
+#             Pct = 100*N/nrow(IDNEW),
+#             Incidents = sum(HXCat12_5_After),
+#             Mean = mean(HXCat12_5_After),
+#             Min = min(HXCat12_5_After),
+#             Q25 = quantile(HXCat12_5_After, probs = 0.25),
+#             Q50 = quantile(HXCat12_5_After, probs = 0.50),
+#             Q75 = quantile(HXCat12_5_After, probs = 0.75),
+#             Max = max(HXCat12_5_After))  %>%
+#   mutate(Category = "Criminal Sexual Conduct (CSC)",
+#          Group = "Sexual Crimes",
+#          SortOrder = 1) ->
+#   HCA_5
+
+IDNEW %>%
+  select(HXCat12_5_After) %>%
+  filter(HXCat12_5_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_5_After))  %>%
+  mutate(Category = "Criminal Sexual Conduct (CSC)",
+         Group = "Sexual Crimes",
+         SortOrder = 1) ->
+  HCA_5
+
+IDNEW %>%
+  select(HXCat12_6_After) %>%
+  filter(HXCat12_6_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_6_After))  %>%
+  mutate(Category = "Drug Crimes",
+         Group = "Other Crimes",
+         SortOrder = 11) ->
+  HCA_6
+
+IDNEW %>%
+  select(HXCat12_7_After) %>%
+  filter(HXCat12_7_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_7_After))  %>%
+  mutate(Category = "Homicide",
+         Group = "Violent Non-Sexual Crimes",
+         SortOrder = 5) ->
+  HCA_7
+
+IDNEW %>%
+  select(HXCat12_8_After) %>%
+  filter(HXCat12_8_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_8_After))  %>%
+  mutate(Category = "Larceny, Theft, Fraud",
+         Group = "Property Crimes",
+         SortOrder = 10) ->
+  HCA_8
+
+IDNEW %>%
+  select(HXCat12_9_After) %>%
+  filter(HXCat12_9_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_9_After))  %>%
+  mutate(Category = "Robbery",
+         Group = "Violent Non-Sexual Crimes",
+         SortOrder = 6) ->
+  HCA_9
+
+IDNEW %>%
+  select(HXCat12_10_After) %>%
+  filter(HXCat12_10_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_10_After)) %>%
+  mutate(Category = "Sex Crimes (non-CSC crimes)",
+         Group = "Sexual Crimes",
+         SortOrder = 2) ->
+  HCA_10
+
+IDNEW %>%
+  select(HXCat12_11_After) %>%
+  filter(HXCat12_11_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_11_After)) %>%
+  mutate(Category = "Traffic & Ordinances",
+         Group = "Other Crimes",
+         SortOrder = 12) ->
+  HCA_11
+
+IDNEW %>%
+  select(HXCat12_12_After) %>%
+  filter(HXCat12_12_After > 0) %>%
+  ungroup() %>%
+  summarise(N = n(),
+            Pct = 100*N/nrow(IDNEW),
+            Incidents = sum(HXCat12_12_After)) %>%
+  mutate(Category = "Weapons",
+         Group = "Violent Non-Sexual Crimes",
+         SortOrder = 7) ->
+  HCA_12
+
+TCap <- paste("Summary of Offenders Arrested, Charged, or Convicted for at",
+              "Least One Incident Occuring After the Testing Window By",
+              "Crime Category")
+FN <- paste("Separately for each crime category, we counted offenders arrested,",
+            "charged, or convicted for at least one incident occurring after",
+            "the testing window. The numerator for each percentage is the",
+            "listed value for n; the denominator is the full sample size (N = ",
+            "1,082). Total incidents are summed across the offenders.")
+
+rbind(HCA_1, HCA_2, HCA_3, HCA_4, HCA_5, HCA_6, HCA_7, HCA_8, HCA_9,
+      HCA_10, HCA_11, HCA_12) %>%
+  arrange(SortOrder) %>%
+  select(Category, N, Pct, Incidents) %>%
+  kable(., format = "html", booktabs = TRUE, digits = 1, row.names = FALSE,
+        col.names = c("Crime Category", "n", "%", "Total Incidents After TW"),
+        format.args = list(big.mark = ','), caption = TCap) %>%
+  kable_styling() %>%
+  add_header_above(c(" " = 1, "Offenders" = 2, " " = 1)) %>%
+  group_rows("Sexual Crimes", 1, 2) %>%
+  group_rows("Violent Non-Sexual Crimes", 3, 7) %>%
+  group_rows("Property Crimes", 8, 10) %>%
+  group_rows("Other Crimes", 11, 12) %>%
+  column_spec(., column = 2, width = "1.15cm") %>%
+  column_spec(., column = 3, width = "1.15cm") %>%
+  column_spec(., column = 4, width = "2cm") %>%
+  footnote(general = FN, general_title = "Note: ", footnote_as_chunk = TRUE,
+           threeparttable = TRUE)
+
+# Graph after period frequency distrbutions
+CCLabels <- c("Criminal Sexual Conduct (CSC)",
+              "Sex Crimes (non-CSC crimes)",
+              "Assault, Domestic Violence",
+              "Assault, non-Domestic Violence",
+              "Homicide",
+              "Robbery",
+              "Weapons",
+              "Arson",
+              "Burglary",
+              "Larceny, Theft, Fraud",
+              "Drug Crimes",
+              "Traffic & Ordinances")
+
+TIDNEW %>%
+  select(HCA_1, HCA_2, HCA_3, HCA_4, HCA_5, HCA_6, HCA_7, HCA_8, HCA_9,
+         HCA_10, HCA_11, HCA_12) %>%
+  pivot_longer(data = ., cols = starts_with("HCA_"), names_to = "Category",
+               values_to = "ICount") %>%
+  mutate(Crime = case_when(Category == "HCA_1" ~ "Arson",
+                           Category == "HCA_2" ~ "Assault, Domestic Violence",
+                           Category == "HCA_3" ~ "Assault, non-Domestic Violence",
+                           Category == "HCA_4" ~ "Burglary",
+                           Category == "HCA_5" ~ "Criminal Sexual Conduct (CSC)",
+                           Category == "HCA_6" ~ "Drug Crimes",
+                           Category == "HCA_7" ~ "Homicide",
+                           Category == "HCA_8" ~ "Larceny, Theft, Fraud",
+                           Category == "HCA_9" ~ "Robbery",
+                           Category == "HCA_10" ~ "Sex Crimes (non-CSC crimes)",
+                           Category == "HCA_11" ~ "Traffic & Ordinances",
+                           Category == "HCA_12" ~ "Weapons")) %>%
+  mutate(Crime = factor(Crime, levels = CCLabels, labels = CCLabels),
+         # Also create CrimeRev where the order of levels is reversed to make it
+         # easy to get a boxplot with the criem categories shown in same order
+         # as we get them for histograms and tables.
+         CrimeRev = factor(Crime, levels = rev(CCLabels),
+                           labels = rev(CCLabels))) ->
+  tmpdf
+
+tmpdf
+
+# Full view (larceny is category w/ largest observed value (ICount = 37)
+histogram(~ ICount | Crime, data = tmpdf, type = "count", as.table = TRUE,
+          # Breaks is set so we get a bin for every integer value of ICount.
+          breaks = -1:38, xlim = c(-1, 38),
+          xlab = "No. incidents after testing window",
+          ylab = "No. of offenders (frequency)")
+
+# Zoom in on ICount values of 0-10
+histogram(~ ICount | Crime, data = tmpdf, type = "count", as.table = TRUE,
+          # Breaks is set so we get a bin for every integer value of ICount.
+          breaks = -1:38, xlim = c(-1, 10),
+          xlab = "No. incidents after testing window",
+          ylab = "No. of offenders (frequency)")
+
+# Zoom in on frequency of 0-300.
+histogram(~ ICount | Crime, data = tmpdf, type = "count", as.table = TRUE,
+          # Breaks is set so we get a bin for every integer value of ICount.
+          breaks = -1:38, ylim = c(0, 300),
+          xlab = "No. incidents after testing window",
+          ylab = "No. of offenders (frequency)")
+
+# U
+bwplot(CrimeRev ~ ICount, data = tmpdf, factor = 1.5, xlim = c(-2, 40),
+       as.table = TRUE,
+       panel = panel.mybox, par.settings = my.boxes,
+       xlab = "No. incidents after testing window")
